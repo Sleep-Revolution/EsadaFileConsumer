9b682d634fc75d8f1ffcbc843e2fdbbdc8bb1c88
benedikt <benedikthth@ru.is>
2023-09-21 16:02:29 +0000

    status messaging

--- Processor.py
+++ Processor.py
@@ -11,120 +11,115 @@ import uuid
 import requests
 creds = pika.PlainCredentials('server', 'server')
 
-# connection_params = pika.ConnectionParameters(os.environ['RABBITMQ_SERVER'], 5672, '/', creds)
-# connection = pika.BlockingConnection(pika.ConnectionParameters(os.environ['RABBITMQ_SERVER'], 5672, '/', creds, heartbeat=60*10))
-# # connection = pika.BlockingConnection(connection_params)
-
-# # connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-# channel = connection.channel()
-# channel.exchange_declare(exchange='progress_topic', exchange_type='topic')
-
 queue_name = 'file_progress_queue'
 # channel.queue_declare(queue=queue_name)
+
+# STATUS_MESSAGES = {
+#     FAIL: -1,
+#     STARTED: 0,
+#     FINISHED: 1,
+# }
+class STATUS_MESSAGES:
+    FAIL = -1
+    STARTED = 0
+    FINISHED = 1 
+    JOBEND = 2
+
 
 
 import datetime
 class ProgressMessage:
-    def __init__(self, stepNumber:int, taskTitle:str, progress:int, message:str=""):
+    def __init__(self, stepNumber:int, taskTitle:str, progress:int, message:str="", fileName: str="", centreId:int=None, datasetName:str=None):
         self.StepNumber = stepNumber
         self.TaskTitle = taskTitle
         self.Progress = progress
+        self.FileName = fileName
         self.Message = message
-    def serialise(self) -> str: 
-        return json.dumps({
-            'stepNumber': self.StepNumber,
-            'taskTitle': self.TaskTitle,
-            'progrees': self.Progress,
-            'message': self.Message
-        })
+        self.CentreId = centreId
+        self.DatasetName = datasetName
+    def serialise(self) -> dict: 
+        return {
+            'CentreId': self.CentreId,
+            'FileName': self.FileName,
+            'StepNumber': self.StepNumber,
+            'TaskTitle': self.TaskTitle,
+            'Progress': self.Progress,
+            'Message': self.Message,
+            'DatasetName': self.DatasetName
+        }
 
-def basicpublish(channel, name, taskNumber, task, status, message=""):
-    channel.basic_publish(
-            exchange='progress_topic',
-            routing_key=f'file_progress.{name}',
-            body=ProgressMessage(taskNumber, task, status, message).serialise()
-        )
-
-
+# def basicpublish(name, taskNumber, task, status=-2, message="", fileName=None, centreId=None, channel=None):
+#     if channel is not None:
+#         channel.basic_publish(
+#                 exchange='progress_topic',
+#                 routing_key=f'file_progress.{name}',
+#                 body=ProgressMessage(taskNumber, task, status, message, centreId).serialise()
+#             )
+#     else:
+#         url = 'http://localhost:8000/meta/log'
+#         entry = ProgressMessage(taskNumber, task, status, message, fileName, centreId)
+#         # print(entry.serialise())
+#         r = requests.post(url, json=entry.serialise())
+#         pass
 
 
 def process_file(channel, message):
+
+
+        
     
     # Centre --:> uploads == [ id int, location, etc. ]
     projectName = str(uuid.uuid4())
     projectLocation = os.path.join('temp_uuids', projectName)
     os.makedirs(projectLocation)
-
     path = message['path'] #centre name
     name = message['name'] # ESR 0xyy0z
-    routing_key = f'file_progress.{name}'
+    centreId = message['centreId']
+    isDataset = message['dataset']
+    datasetName = None if not isDataset else path
+    step = -100
+    task = "preparatory task"
+
+    def basicpublish(status=-2, message=""):
+        url = 'http://localhost:8000/meta/log'
+        entry = ProgressMessage(step, task, status, message, name, centreId, datasetName=datasetName)
+        print(entry.serialise())
+        r = requests.post(url, json=entry.serialise())
+        pass
+
     # BUCKET/CENTRE/NAME/
-    receivedLocation = os.path.join(os.environ['INDIVIDUAL_NIGHT_WAITING_ROOM'], path, name)
-
-    print('------->', routing_key, flush=True)
-    # Download the file from the location specified in the message
-    exchange_name = os.environ['PROGRESS_EXCHANGE_NAME']
-    exchange_type = 'topic'
-    channel.exchange_declare(exchange=exchange_name, exchange_type=exchange_type)
-    channel.queue_bind(queue=queue_name, exchange=exchange_name, routing_key=routing_key)
-
-    notes = []
-
-
-
+    receivedLocation = ""
+    if isDataset:
+        receivedLocation = os.path.join(os.environ['DATASET_DIR'], path, name)
+    else:
+        receivedLocation = os.path.join(os.environ['INDIVIDUAL_NIGHT_WAITING_ROOM'], path, name)
+        # Download the file from the location specified in the message
     step = 1 
     task = 'Convert To EDF'
-    basicpublish(channel, name, step, task, 0)
-    # receivedZipFiles = list(filter(lambda x: '.zip' in x, os.listdir(receivedLocation)))
-    # if len(receivedZipFiles) != 1:
-    #     basicpublish(channel, name, step, task, 2, "Number of received files from the Nox EDF service was not equal to 1") 
-    #     raise Exception(f"Failed task {step}, \"{task}\"")
-    # receivedZipFile = receivedZipFiles[0]
-    # originalZipLocation = os.path.join(receivedLocation, receivedZipFile)
+    
+    
 
+    # channel.queue_bind(queue=queue_name, exchange=exchange_name, routing_key=routing_key)
+    notes = []
+    basicpublish(status=STATUS_MESSAGES.STARTED)
     
     Success, Message, edfName = NoxToEdf(receivedLocation, projectLocation)
-    
-
-
     if not Success:
-        basicpublish(channel, name, step, task, 2, Message)
-        raise Exception(f"Failed task {step}, \"{task}\", reason given was \"{Message}\"")
-    basicpublish(channel, name, step, task, 1)
-
-
-    # step = step + 1
-    # task = 'Extract Original Zip file to temporary destination'
-    # basicpublish(channel, name, step, task, 0)
-    # unzipLocation = os.path.join(projectLocation, 'unzipped_original_recording')
-    # try:
-    # # Extract the zip file to the destination folder.
-    #     print("\t -> Extracting Zipped NOX folder")
-
-    #     with zipfile.ZipFile(originalZipLocation, 'r') as f:
-    #         f.extractall(unzipLocation)
-    #         f.close()
-    #     print("\t <- Done extracting Zipped NOX folder into temporary destination", unzipLocation)
-    #     if len(os.listdir(unzipLocation)) != 1:
-    #         basicpublish(channel, name, step, task, 2, 'Bad number of folders inside extracted nox recording!')
-    #         raise Exception(f"Failed task {step}, \"{task}\"")
-            
-    # except:
-    #     basicpublish(channel, name, step, task, 2,  f'Failed to extract the ZIP recording in {originalZipLocation} to {unzipLocation}!')
-    #     raise Exception(f"Failed task {step}, \"{task}\"")
-
-    # newFolder = os.listdir(unzipLocation)[0]
-    # receivedRecordingLocation = os.path.join(unzipLocation, newFolder)
-    # basicpublish(channel, name, step, task, 1)
-
+        basicpublish(status=STATUS_MESSAGES.FAIL, message=Message)
+        # basicpublish(channel,name,setp,task, -1, f"Failed task {step}, \"{task}\", reason given was \"{Message}\"")
+        return
+        # raise Exception(f"Failed task {step}, \"{task}\", reason given was \"{Message}\"")
+    
+    basicpublish(status=STATUS_MESSAGES.FINISHED)
 
     step = step + 1
     task = "Get json from original ndb file"
     files = os.listdir(receivedLocation)
     oldNdbFiles = list(filter(lambda x: '.ndb' in x.lower(), files))
     if len(oldNdbFiles) != 1:
-        basicpublish(channel, name, step, task, 2, f"Found {len(oldNdbFiles)} ndb files in extracted location.")
-        raise Exception("Found too many ndb recordings.")
+        basicpublish(status=STATUS_MESSAGES.FAIL, message=f"Found {len(oldNdbFiles)} ndb files in extracted location.")
+        return
+        # raise Exception(f"Found {len(oldNdbFiles)} ndb recordings. (expected 1)")
     oldNdbFileLocation = os.path.join(receivedLocation, oldNdbFiles[0])
     files = {'ndb_file': open(oldNdbFileLocation, 'rb').read()}
     headers = {
@@ -139,32 +134,30 @@ def process_file(channel, message):
         now = datetime.datetime.now()
         print("\t -> Posting NDB to NDB->JSON service", flush=True)
         r = requests.post(f'{os.environ["NOX_NDB_SERVICE"]}/ndb-to-json', files=files, headers=headers)
-        print(r.content)
         scoringJson = json.loads(r.content)
         print("\t <- Done posting Nox zip to service", flush=True)
         print(f"\t <-- It took {datetime.datetime.now() - now} seconds....", flush=True)
     except Exception as e:
-        print("fuc,", e)
+        print("Raised Exception while Posting ndb to json,", e)
     if scoringJson != None:
-        print("Did not get a scoring json.")
+        print("Got a scoring json.")
         if len(scoringJson['active_scoring_name']) == 0:
             scoringJson['active_scoring_name'] = "default-scoring-1"
         for i in range(len(scoringJson['scorings'])):
             if scoringJson['scorings'][i]['scoring_name'] == "":
                 scoringJson['scorings'][i]['scoring_name'] = f"default-scoring-{i+1}"
 
-    basicpublish(channel, name, step, task, 1)
-
-
+    
+    basicpublish(status=STATUS_MESSAGES.FINISHED)
 
     # Run matias algorithm
     step = step + 1
     task = 'Run Matias Algorithm'
-    basicpublish(channel, name, step, task, 0)
+    basicpublish(status=STATUS_MESSAGES.STARTED)
     Success, Message, JSONMatias = RunMatiasAlgorithm(os.path.join(projectLocation, edfName))
     # print(JSONMatias)
     if not Success:
-        basicpublish(channel, name, step, task, 2, Message)
+        basicpublish(status=STATUS_MESSAGES.FAIL, message=Message)
         notes.append("Failed to run Matias algorithm")
     else:
         # raise Exception(f"Failed task {step}, \"{task}\"")
@@ -173,54 +166,48 @@ def process_file(channel, message):
         else:
             Success, Message, scoringJson = JSONMerge(scoringJson, JSONMatias)
             if not Success:
-                raise Exception(f"Failed task {step}, \"{task}\", reason given was \"{Message}\"")
-        basicpublish(channel, name, step, task, 1)
-        
-
-
+                basicpublish(status=STATUS_MESSAGES.FAIL, 
+                        message=f"Failed task {step}, \"{task}\", reason given was \"{Message}\""
+                    )
+                return
+        basicpublish(status=STATUS_MESSAGES.FINISHED)
 
     print("Running Nox SAS service.", flush=True)
     step = step + 1
     task = 'Run NOX SAS Service'
-    basicpublish(channel, name, step, task, 0)
+    basicpublish(status=STATUS_MESSAGES.STARTED)
     Success, Message, JSONNox = RunNOXSAS(receivedLocation)
     if not Success:
-        basicpublish(channel, name, step, task, 2, Message)
+        basicpublish(status=STATUS_MESSAGES.FAIL, message=Message)
         notes.append(f"Failed task {step}, \"{task}\"")
     else:
         Success, Message, scoringJson = JSONMerge(scoringJson, JSONNox)
         if not Success:
-            raise Exception(f"Failed task {step}, \"{task}\", reason given was \"{Message}\"")
-        basicpublish(channel, name, step, task, 1)
+            #raise Exception(f"Failed task {step}, \"{task}\", reason given was \"{Message}\"")
+            basicpublish(status=STATUS_MESSAGES.FAIL, 
+                    message=f"Failed task {step}, \"{task}\", reason given was \"{Message}\"",
+                )
+            return
+        basicpublish(status=STATUS_MESSAGES.FINISHED)
     
-
-    # step = step + 1
-    # task = 'Combine JSON'
-    # basicpublish(channel, name, step, task, 0)
-    # Success, Message, JSONM = JSONMerge(JSONMatias,JSONNox)
-    # if not Success:
-    #     basicpublish(channel, name, step, task, 2, Message)
-    #     raise Exception(f"Failed task {step}, \"{task}\"")
-    # basicpublish(channel, name, step, task, 1)
     j = json.dumps(scoringJson)
     step = step + 1
     task = 'Get NDB'
-    basicpublish(channel, name, step, task, 0)
+    basicpublish(status=STATUS_MESSAGES.STARTED)
     Success, Message, ndbDestination = JsonToNdb(scoringJson, projectLocation)
     if not Success:
-        basicpublish(channel, name, step, task, 2, Message)
-        raise Exception(f"Failed task {step}, \"{task}\", reason given was \"{Message}\"")
-    basicpublish(channel, name, step, task, 1)
-
+        # basicpublish(channel, name, step, task, 2, Message)
+        # raise Exception(f"Failed task {step}, \"{task}\", reason given was \"{Message}\"")
+        basicpublish(name, step,task, 
+            status=STATUS_MESSAGES.FAIL, 
+            message=f"Failed task {step}, \"{task}\", reason given was \"{Message}\"", 
+            fileName=name, centreId=centreId)
+        return
+    basicpublish(status=STATUS_MESSAGES.FINISHED)
     
-
-    # move all files inside the new folder in "unzipped_original_recording"
-    
-
-    # os.mkdir(centreDestinationFolder)
     centreDestinationFolder = os.path.join(os.environ['DELIVERY_FOLDER'], path)
     if not os.path.exists(centreDestinationFolder):
-        os.mkdir(centreDestinationFolder)
+        os.makedirs(centreDestinationFolder)
     processedRecordingFolder = os.path.join(centreDestinationFolder, name)
     if not os.path.exists(processedRecordingFolder):
         os.makedirs(processedRecordingFolder)
@@ -231,19 +218,31 @@ def process_file(channel, message):
     for file in files:
         if '.ndb' in file.lower():
             continue
-        shutil.copy(
-            os.path.join(receivedLocation, file),
-            processedRecordingFolder
-        )
+        shutil.copy( os.path.join(receivedLocation, file), processedRecordingFolder)
     
+
+    # now deliver to teh edf folder.
+    edfDeliveryfolder = os.path.join(os.environ["EDF_DELIVERY_FOLDER"], path)
+    if not os.path.exists(edfDeliveryfolder):
+        os.makedirs(edfDeliveryfolder)
+
+    shutil.copy(
+        os.path.join(projectLocation, edfName),
+        edfDeliveryfolder
+        )
+    jsonName = edfName.replace('.edf', '.scoring.json')
+    #writie a code that writes the json object scoringJsoninto a file called jsonName in the folder edfDeliveryFolder
+    json_string = json.dumps(scoringJson)
+
+    # Write the JSON string to the file
+    with open(os.path.join(edfDeliveryfolder, jsonName), "w") as file:
+        file.write(json_string)
+    # clean up
     shutil.rmtree(projectLocation)
 
-
-# def on_message(channel, method, properties, body):
-#     print("Recived a message")
-    
-    
-
+    step += 1
+    task = "Finished"
+    basicpublish(STATUS_MESSAGES.JOBEND, "Finished job.")
 
 # def consume_queue1():
 #     # connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
@@ -279,15 +278,6 @@ def consume_queue2():
     channel.basic_qos(prefetch_count=1)
     channel.basic_consume(queue=os.environ['TASK_QUEUE'], on_message_callback=callback)
     channel.start_consuming()
-
-
-# channel.basic_qos(prefetch_count=1)
-# channel.basic_consume(queue=os.environ['TASK_QUEUE'], on_message_callback=on_message)
-
-# print("Now consuming from channel.")
-# channel.start_consuming()
-# connection.close()
-
 
 if __name__ == '__main__':
     print("Starting consumer threads")
